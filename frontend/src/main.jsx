85fed59f174e2317060cef6f7ae5d6fba91eadc1
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-09-08 01:28:55 +0530

    ' Commit Description: ' ' Set up the main entry point for the React application with Chakra UI: ' ' - Imported necessary modules from React and Chakra UI. ' - Configured global styles using Chakra's theme tools to handle '   light and dark modes. ' - Extended the Chakra theme to include custom color settings '   for gray shades. ' - Implemented StrictMode for highlighting potential problems '   in the application. ' - Rendered the App component wrapped in ChakraProvider to '   apply the theme and manage color modes. ' ' This setup enhances the application's UI consistency and ' responsiveness to user preferences regarding color modes.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/theme-utils';
 import './index.css'
+
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('gray.100', '#101010')(props),
+    }
+  })
+}
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+}
+
+const color = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e",
+  }
+}
+
+const theme = extendTheme({ config, styles, color });
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>,
