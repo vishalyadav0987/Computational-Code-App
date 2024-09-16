bcb2a70e742d74fab6ee835daca36ef0b261d0e4
vishalyadav0987 <viahalyadav0987@gmail.com>
2024-09-16 10:48:29 +0530

    ' PushProjectComponentModule.vb ' ' This file contains the implementation of the PushProject component for submitting new projects. ' ' Commit Message: ' feat: Implement PushProject Component ' - Created the PushProject component to handle the submission of new project data. ' - Integrated usePreviewImage custom hook to manage image uploads and previews. ' - Utilized Redux for state management, dispatching actions to push new projects and handle errors. ' - Implemented form handling to capture project details, including title, URLs, and related technologies. ' - Used FormData to construct the request payload, allowing for image uploads alongside project data. ' - Added toast notifications to provide user feedback for errors and successful project submissions. ' - Reset the project creation state upon successful submission and closed the modal. ' ' The PushProject component streamlines the process of submitting new projects, ' ensuring a user-friendly interface for project management within the application.

--- frontend/src/Pages/PushProject/PushProject.jsx
+++ frontend/src/Pages/PushProject/PushProject.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {
     Modal,
     ModalOverlay,
@@ -30,129 +30,202 @@ import {
     Text
 } from '@chakra-ui/react'
 import { FaFileUpload } from 'react-icons/fa'
-import { CheckIcon, SmallCloseIcon } from '@chakra-ui/icons'
 import { MdLink, MdOutlineTitle } from "react-icons/md";
 import { TbCirclesRelation } from "react-icons/tb";
 import { GoRepoPush } from "react-icons/go";
-
-const imag = ""
+import usePreviewImage from '../../useCustomHook/usePreviewImage'
+import { useDispatch, useSelector } from 'react-redux'
+import { clearError, pushProjectK } from '../../redux/actions/devspaceActions';
+import toast from 'react-hot-toast';
+import { PUSH_NEW_PROJECT_RESET } from '../../redux/constants/DevspaceConstants';
 
 const PushProject = ({ isOpen, onClose }) => {
-    const fileRef = useRef(null)
+    const fileRef = useRef(null);
+    const dispatch = useDispatch();
+    const { error, loading, message, isCreated } = useSelector(state => state.pushProject)
+    const { imageChangeHandle, imageUrl, setImageUrl } = usePreviewImage()
+    const [projectData, setProjectData] = useState({
+        projectTitle: "",
+        websiteUrl: "",
+        githubUrl: "",
+        projectRelatedTo: "",
+    });
+
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+        setProjectData((prev) => ({
+            ...prev,   // Keep the existing state
+            [name]: value,  // Update the specific field by name
+        }));
+    };
+
+    const pushProjectHandler = async (e) => {
+        console.log("click")
+        e.preventDefault();
+        const formData = new FormData();
+        formData.set("projectTitle", projectData.projectTitle)
+        formData.set("websiteUrl", projectData.websiteUrl)
+        formData.set("githubUrl", projectData.githubUrl)
+        formData.set("projectRelatedTo", projectData.projectRelatedTo)
+
+        if (imageUrl) {
+            formData.append("frontImage", imageUrl);
+        }
+
+        dispatch(pushProjectK(formData));
+
+    }
+    useEffect(() => {
+        if (error) {
+            toast.error(error);
+            dispatch(clearError());
+        }
+        if (isCreated) {
+            toast.success(message);
+            dispatch({ type: PUSH_NEW_PROJECT_RESET })
+            onClose();
+        }
+
+    }, [isCreated, error, dispatch])
+
 
     return (
-        <Modal isOpen={isOpen} onClose={onClose} isCentered size="full">
+        <Modal isOpen={isOpen} onClose={onClose} isCentered size="full" >
             <ModalOverlay />
-            <ModalContent >
-                <ModalHeader>Upload Project</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody width={"100%"}>
-                    <Flex width={"80%"}
-                        margin={"0 auto"}
-                        p={10} py={16} gap={4} alignItems={"start"}>
-                        <Box
-                            cursor={"pointer"}
-                            border={"1px dashed #c7c7c7"}
-                            width={"400px"} height={"400px"}
+            <ModalContent position={"relative"}>
+                <form >
+                    <ModalHeader>Upload Project</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody width={"100%"}>
+                        <Flex width={"80%"}
+                            margin={"0 auto"}
+                            p={10} py={16} gap={4} alignItems={"start"}>
+                            <Box
+                                onClick={() => fileRef.current.click()}
+                                cursor={"pointer"}
+                                border={"1px dashed #c7c7c7"}
+                                width={"400px"} height={"400px"}
+                                display={"flex"}
+                                alignItems={"center"}
+                                justifyContent={"center"}
+                            >
+                                {imageUrl && <Flex>
+                                    <Image
+                                        rounded={'md'}
+                                        alt={'feature image'}
+                                        src={imageUrl}
+                                        objectFit={'cover'}
+                                    />
+                                </Flex>}
+                                {!imageUrl && (
+                                    <>
+                                        <FaFileUpload
+                                            fontSize={50}
+                                        />
+                                        <Text fontWeight={"bold"} size={"md"}>Upload Fornt Page</Text>
+                                    </>
+                                )}
+                                <Input type='file' hidden ref={fileRef} onChange={imageChangeHandle} />
+                            </Box>
+
+                            <Flex
+                                // minH={'100vh'}
+                                width={"50%"}
+                                align={'center'}
+                                justify={'center'}
+                            // bg={useColorModeValue('gray.50', 'gray.800')}
+                            >
+                                <Stack
+                                    spacing={4}
+                                    w={'full'}
+                                    maxW={'lg'}
+
+                                    rounded={'xl'}
+                                >
+
+                                    <Heading>Project Detail</Heading>
+                                    <FormControl id="userName" isRequired>
+                                        <FormLabel>Title</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement
+                                                pointerEvents='none' color='gray.300' fontSize='1.2em'>
+                                                <MdOutlineTitle />
+                                            </InputLeftElement>
+                                            <Input placeholder='Project Title'
+                                                name='projectTitle'
+                                                onChange={onChangeHandler}
+                                                value={projectData.projectTitle}
+                                            />
+                                        </InputGroup>
+                                    </FormControl>
+                                    <FormControl id="text" isRequired>
+                                        <FormLabel>Webiste URL</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement
+                                                pointerEvents='none' color='gray.300' fontSize='1.2em'>
+                                                <MdLink />
+                                            </InputLeftElement>
+                                            <Input placeholder='Website url'
+                                                name='websiteUrl'
+                                                onChange={onChangeHandler}
+                                                value={projectData.websiteUrl}
+                                            />
+                                        </InputGroup>
+                                    </FormControl>
+                                    <FormControl id="password" isRequired>
+                                        <FormLabel>Github URL</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement
+                                                pointerEvents='none' color='gray.300' fontSize='1.2em'>
+                                                <MdLink />
+                                            </InputLeftElement>
+                                            <Input
+                                                placeholder='ttps://github.com/vishalyadav0987/Job-Apply'
+                                                name='githubUrl'
+                                                onChange={onChangeHandler}
+                                                value={projectData.githubUrl}
+                                            />
+                                        </InputGroup>
+
+                                    </FormControl>
+                                    <FormControl id="password" isRequired>
+                                        <FormLabel>Related To</FormLabel>
+                                        <InputGroup>
+                                            <InputLeftElement
+                                                pointerEvents='none' color='gray.300' fontSize='1.2em'>
+                                                <TbCirclesRelation />
+                                            </InputLeftElement>
+                                            <Input
+                                                placeholder='mern-stack'
+                                                name='projectRelatedTo'
+                                                onChange={onChangeHandler}
+                                                value={projectData.projectRelatedTo}
+                                            />
+                                        </InputGroup>
+
+                                    </FormControl>
+
+                                </Stack>
+                            </Flex>
+
+                        </Flex>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button bg={"#ed8936"}
+                            isLoading={loading}
                             display={"flex"}
                             alignItems={"center"}
-                            justifyContent={"center"}
+                            gap={2}
+                            position={"absolute"}
+                            bottom={"10px"}
+                            onClick={pushProjectHandler}
                         >
-                            {imag && <Flex>
-                                <Image
-                                    rounded={'md'}
-                                    alt={'feature image'}
-                                    src={
-                                        'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-                                    }
-                                    objectFit={'cover'}
-                                />
-                            </Flex>}
-                            <FaFileUpload
-                                onClick={() => fileRef.current.click()}
-                                fontSize={50}
-                            />
-                            <Text fontWeight={"bold"} size={"md"}>Upload Fornt Page</Text>
-                            <Input type='file' hidden ref={fileRef} />
-                        </Box>
-
-                        <Flex
-                            // minH={'100vh'}
-                            width={"50%"}
-                            align={'center'}
-                            justify={'center'}
-                        // bg={useColorModeValue('gray.50', 'gray.800')}
-                        >
-                            <Stack
-                                spacing={4}
-                                w={'full'}
-                                maxW={'lg'}
-
-                                rounded={'xl'}
-                            >
-
-                                <Heading>Project Detail</Heading>
-                                <FormControl id="userName" isRequired>
-                                    <FormLabel>Title</FormLabel>
-                                    <InputGroup>
-                                        <InputLeftElement
-                                            pointerEvents='none' color='gray.300' fontSize='1.2em'>
-                                            <MdOutlineTitle />
-                                        </InputLeftElement>
-                                        <Input placeholder='Project Title' />
-                                    </InputGroup>
-                                </FormControl>
-                                <FormControl id="text" isRequired>
-                                    <FormLabel>Webiste URL</FormLabel>
-                                    <InputGroup>
-                                        <InputLeftElement
-                                            pointerEvents='none' color='gray.300' fontSize='1.2em'>
-                                            <MdLink />
-                                        </InputLeftElement>
-                                        <Input placeholder='Website url' />
-                                    </InputGroup>
-                                </FormControl>
-                                <FormControl id="password" isRequired>
-                                    <FormLabel>Github URL</FormLabel>
-                                    <InputGroup>
-                                        <InputLeftElement
-                                            pointerEvents='none' color='gray.300' fontSize='1.2em'>
-                                            <MdLink />
-                                        </InputLeftElement>
-                                        <Input
-                                            placeholder='ttps://github.com/vishalyadav0987/Job-Appl' />
-                                    </InputGroup>
-
-                                </FormControl>
-                                <FormControl id="password" isRequired>
-                                    <FormLabel>Related To</FormLabel>
-                                    <InputGroup>
-                                        <InputLeftElement
-                                            pointerEvents='none' color='gray.300' fontSize='1.2em'>
-                                            <TbCirclesRelation />
-                                        </InputLeftElement>
-                                        <Input
-                                            placeholder='ttps://github.com/vishalyadav0987/Job-Appl' />
-                                    </InputGroup>
-
-                                </FormControl>
-
-                            </Stack>
-                        </Flex>
-
-                    </Flex>
-                </ModalBody>
-
-                <ModalFooter>
-                    <Button bg={"#ed8936"}
-                        display={"flex"}
-                        alignItems={"center"}
-                        gap={2}
-                    >
-                        <GoRepoPush />
-                        <Text>Push</Text>
-                    </Button>
-                </ModalFooter>
+                            <GoRepoPush />
+                            <Text>Push</Text>
+                        </Button>
+                    </ModalFooter>
+                </form>
             </ModalContent>
         </Modal>
     )
